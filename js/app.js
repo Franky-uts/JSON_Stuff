916756f963f9e97814fe99c3b77ce6ceebf15b1e
Franky-uts <107886319+Franky-uts@users.noreply.github.com>
2022-10-06 13:10:00 -0500

    Se agregó mas información

--- js/app.js
+++ js/app.js
@@ -7,6 +7,26 @@ document.addEventListener('DOMContentLoaded', () =>{
     ObtenerUsuarios()
 })
 
+const pintarCards = () => {
+  usuario.forEach(user => {
+      templateCard.querySelector('.card-header').textContent = user.name
+      templateCard.querySelectorAll('h7')[0].textContent = user.email
+      templateCard.querySelectorAll('h7')[1].textContent = user.phone
+      templateCard.querySelectorAll('h7')[2].textContent = user.name
+      templateCard.querySelectorAll('h7')[3].textContent = user.website
+      templateCard.querySelectorAll('h7')[4].textContent = user.address.street
+      templateCard.querySelectorAll('h7')[5].textContent = user.address.suite
+      templateCard.querySelectorAll('h7')[6].textContent = user.address.city
+      templateCard.querySelectorAll('h7')[7].textContent = user.address.zipcode
+      templateCard.querySelectorAll('h7')[8].textContent = user.company.name
+      templateCard.querySelectorAll('h7')[9].textContent = user.company.bs
+      templateCard.querySelector('.btn-primary').dataset.id = user.id
+      const clone = templateCard.cloneNode(true)
+      fragment.appendChild(clone)
+  })
+  usuariosHTML.appendChild(fragment)
+}
+
 function ObtenerUsuarios () {
   fetch('https://jsonplaceholder.typicode.com/users')
   .then(async (data) => {
